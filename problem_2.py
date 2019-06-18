6effa3fb4e71b309776caa98a989223349ac0804
Daniel Loaiza <daniel.loaiza@globant.com>
2019-06-18 13:02:38 -0500

    solved problem 2

--- problem_2.py
+++ problem_2.py
@@ -1,58 +1,36 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[35]:
+# In[13]:
 
 
-def rotated_array_search(input_list, number):
-    """
-    Find the index by searching in a rotated sorted array
-
-    Args:
-       input_list(array), number(int): Input array to search and the target
-    Returns:
-       int: Index or -1
-    """
-    max = len(input_list) -1
-    min = 0
+# Returns index of key in arr[l..h] if key is present, 
+# otherwise returns -1 
+def rotated_array_search (arr, number):
+    def search (arr, low, high, key):
+        if low > high: 
+            return -1
+      
+        mid = (low + high) // 2
+        if arr[mid] == key: 
+            return mid 
+  
+        # If arr[l...mid] is sorted  
+        if arr[l] <= arr[mid]: 
+  
+            # As this subarray is sorted, we can quickly 
+            # check if key lies in half or other half  
+            if key >= arr[l] and key <= arr[mid]: 
+                return search(arr, l, mid-1, key) 
+            return search(arr, mid+1, h, key) 
+  
+        # If arr[l..mid] is not sorted, then arr[mid... r] 
+        # must be sorted 
+        if key >= arr[mid] and key <= arr[h]: 
+            return search(arr, mid+1, h, key) 
+        return search(arr, l, mid-1, key)
     
-    rotatedIndex = -1
-    
-    while(min <= max):
-        mid = (max + min) // 2
-        if(number == input_list[mid]):
-            return mid
-        else:
-            #check two possible pivots
-            left_max = mid
-            left_mid = (left_max + min) //2
-            left_element = input_list[left_mid]
-            
-            right_min = mid
-            right_mid = (right_min + max)//2
-            right_element = input_list[right_mid]
-            
-            # there is an inversion
-            if(left_element > right_element):
-                right_minimum =  right_element - (right_mid -  mid) + 1
-                right_max = right_element + (max - right_mid)
-                
-                if(is_between(number, right_minimum, right_max)):
-                    min = mid + 1
-                else:
-                    max = mid - 1
-                    
-            else:
-                #normal search
-                if(number > input_list[mid]):
-                    min = mid + 1
-                else:
-                    max = mid - 1
-    return -1   
-    pass
-
-def is_between(number, min, max):
-    return number >= min and number <= max
+    return search(arr, 0, len(arr)-1 , number)
 
 def linear_search(input_list, number):
     for index, element in enumerate(input_list):
@@ -72,7 +50,7 @@ test_function([[5, 6, 7, 0, 1, 2, 3, 4], 4])
 test_function([[4, 5, 6, 7, 0, 1, 2], 4])
 test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
 test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 8])
+test_function([[7, 8, 10, 12, 1, 2, 4, 6], 10])
 test_function([[6, 7, 8, 1, 2, 3, 4], 1])
 test_function([[6, 7, 8, 1, 2, 3, 4], 10])
 
