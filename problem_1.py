2d597b285ed74148ffd8da5621049d908fe68333
Daniel Loaiza <danielfelipeloaiza@hotmail.com>
2019-06-18 21:55:03 -0500

    Update problem_1.py

--- problem_1.py
+++ problem_1.py
@@ -9,12 +9,13 @@ import math
 def sqrt(number):
     """
     Calculate the floored square root of a number
-
     Args:
        number(int): Number to find the floored squared root
     Returns:
        int: Floored Square Root
     """
+    if(number < 0):
+        return None
     if(number == 0):
         return 0
     if(number == 1):
@@ -38,9 +39,9 @@ def sqrt(number):
             
     pass
 
+print ("Pass" if  (None == sqrt(-9)) else "Fail")
 print ("Pass" if  (3 == sqrt(9)) else "Fail")
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
 print ("Pass" if  (4 == sqrt(16)) else "Fail")
 print ("Pass" if  (1 == sqrt(1)) else "Fail")
 print ("Pass" if  (5 == sqrt(27)) else "Fail")
-
